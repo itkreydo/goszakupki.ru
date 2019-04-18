69d6cf91397da8eb5ac7177296c5e4b0dba90979
itkreydo <it.kreydo@gmail.com>
2019-04-18 22:37:36 +0300

    captcha function

--- send.php
+++ send.php
@@ -5,23 +5,8 @@ require("func.php");
 $act = $_GET["act"];
 switch ($act){
     case 'reg':
-        $captchaKey = "6LcjCp8UAAAAAFonnxjSuk74QHaTebJpgN8ljvJ3";
-          $response = $_POST["g-recaptcha-response"];
-          $url = 'https://www.google.com/recaptcha/api/siteverify';
-          $data = [
-            'secret' => $captchaKey,
-            'response' => $_POST["g-recaptcha-response"]
-          ];
-          $options = [
-            'http' => [
-              'method' => 'POST',
-              'content' => http_build_query($data)
-            ]
-          ];
-          $context  = stream_context_create($options);
-          $verify = file_get_contents($url, false, $context);
-          $captcha_success=json_decode($verify);
-          if ($captcha_success->success==false) {
+
+          if (captchaConfirm()==false) {
             echo "Вы робот, доступ запрещён!";
             exit();
           } 
