bc040d5111262c801643c8ccaa81ab4dbad67863
Venera <Venera060698@yandex.ru>
2019-04-18 22:51:52 +0400

    adminpanel

---
+++ adminpanel/index.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+$act=$_GET['act'];
+switch ($act){
+    case 'reg':
+        require('../templates/header.php');
+        require('templates/reg.php');
+        require('../templates/footer.php');
+    break;
+    case 'login':
+        require('../templates/header.php');
+        require('templates/login.php');
+        require('../templates/footer.php');
+    break;
+    default:
+        require('../templates/header.php');
+        require('templates/main.php');
+        require('../templates/footer.php');
+    break;
+}
+
+?>
