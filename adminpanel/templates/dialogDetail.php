fabe498a16ede6d3c008f628ae613960f1d63a02
prestleeey <shadrindmitry1337@gmail.com>
2019-04-20 14:15:21 +0300

    dialog detail logic

--- adminpanel/templates/dialogDetail.php
+++ adminpanel/templates/dialogDetail.php
@@ -6,7 +6,7 @@
             <div class="chartjs-size-monitor-shrink" style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"><div style="position:absolute;width:200%;height:200%;left:0; top:0"></div></div></div>
     <div class="main_container">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Диалоги</h1>
+        <h1 class="h2">Тема: <?=$result[0]["reason"];?></h1>
         <div class="btn-toolbar mb-2 mb-md-0">
           <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-calendar"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
@@ -15,18 +15,19 @@
         </div>
       </div>
         <div class="">
+            <?php for($i=0;$i<count($result);$i++){ ?>
             <div class="row">
-            <div class="card" style="float:left; margin: 10px; ">
+            <div class="card" style="float:left; margin: 10px;<?php if($result[$i]["sender"]=="Техподдержка") echo "background: #eff1ec;";?> ">
               <div class="card-body" style="float:right;color:#777; ">
                   <div class="row">
                     <div class="col-3">
                       <div class="">
                         <div class="black_bold_title">
-                            <b> ОАО "АВТОВАЗ"</b>
+                            <b> <?php echo $result[$i]["sender"] ?></b>
                           </div>
                           <div class="item_date_block">
                             <div class="gray_title">
-                                  20.05.2019 15:00
+                                  <?php echo $result[$i]["date"]; ?>
                             </div>
                           </div>
                       </div>
@@ -35,7 +36,7 @@
                     <div class="col-6">
                       <div class="">
                           <div class="" style="color:#555;">
-                          Здравствуйте! Не могу произвести оплату на сайте, выдает ошибку, подскажите, пожалуйста, что делать.
+                          <?php echo $result[$i]["text"]; ?>
                           </div>
 
                       </div>
@@ -44,36 +45,7 @@
                 </div>
             </div>
             </div>
-
-            <div class="row">
-            <div class="col-3"></div>
-             <div class="card" style="float:right; margin: 10px;background: #eff1ec;">
-              <div class="card-body" style="float:right;color:#777; ">
-                  <div class="row">
-                    <div class="col-3">
-                      <div class="">
-                        <div class="black_bold_title">
-                            <b>Техподдержка</b>
-                          </div>
-                          <div class="item_date_block">
-                            <div class="gray_title">
-                                  20.05.2019 15:00
-                            </div>
-                          </div>
-                      </div>
-                      </div>
-                    <div class="col-6">
-                      <div class="">
-                          <div class="" style="color:#555;">
-                          Здравствуйте! Не могу произвести оплату на сайте, выдает ошибку, подскажите, пожалуйста, что делать.
-                          </div>
-
-                      </div>
-                    </div>
-                  </div>
-                </div>
-            </div>
-            </div>
+            <?php } ?>
       
             <div class="row" style="margin-top: 20px;padding-top:20px;border-top:1px dotted #ccc">   
                 <div class="col-8">
