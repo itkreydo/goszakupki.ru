3b1b4cbe0f209013e1cea14cf17d61d340a7bbbd
itkreydo <it.kreydo@gmail.com>
2019-04-20 10:58:25 +0300

    bd

--- goszakupki.sql
+++ goszakupki.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Апр 19 2019 г., 22:07
+-- Время создания: Апр 20 2019 г., 10:57
 -- Версия сервера: 5.5.53
 -- Версия PHP: 5.6.29
 
@@ -87,6 +87,72 @@ CREATE TABLE `orgs` (
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `rnp`
+--
+
+CREATE TABLE `rnp` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `date_start` date NOT NULL,
+  `date_end` date NOT NULL,
+  `reason` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `rnp`
+--
+
+INSERT INTO `rnp` (`id`, `id_user`, `date_start`, `date_end`, `reason`) VALUES
+(1, 13, '2019-04-20', '2019-05-31', 'Просто так'),
+(2, 1, '2019-04-20', '2019-04-30', 'Причина 2');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `support`
+--
+
+CREATE TABLE `support` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_admin` int(11) NOT NULL,
+  `reason` varchar(255) NOT NULL,
+  `date_created` date NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `support`
+--
+
+INSERT INTO `support` (`id`, `id_user`, `id_admin`, `reason`, `date_created`, `status`) VALUES
+(1, 1, 1, 'Не работает сайт, пропали данные', '2019-06-30', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `support_chat`
+--
+
+CREATE TABLE `support_chat` (
+  `id` int(11) NOT NULL,
+  `sender` varchar(255) NOT NULL,
+  `text` text NOT NULL,
+  `date` date NOT NULL,
+  `id_support` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `support_chat`
+--
+
+INSERT INTO `support_chat` (`id`, `sender`, `text`, `date`, `id_support`) VALUES
+(1, '\"Пользователь\"', 'Ничего не работает, помогите!', '2019-07-31', 1),
+(2, '\"Пользователь\"', 'А темерь ячто-то нажал и всё сломалось, как подать заявку на гос заказ??', '2019-11-30', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `ur_face`
 --
 
@@ -127,23 +193,28 @@ CREATE TABLE `user` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `date_reg` date NOT NULL,
-  `rnp` int(11) NOT NULL
+  `rnp` int(11) NOT NULL,
+  `token` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `user`
 --
 
-INSERT INTO `user` (`id`, `fio`, `inn`, `phone`, `type`, `email`, `password`, `date_reg`, `rnp`) VALUES
-(1, 'Крейдо Игорь Витальевич', '21321321632142', '', '', 'autodesk@list.ru', '', '2019-04-19', 0),
-(2, 'jzh f difj sd hf ', '82327647253465', '', 'fizFace', 'ksjdshfkjask@jdkjf.ru', '', '2019-04-19', 0),
-(3, 'Крейдо Игорь Витальевич', '', '', 'urFace', 'asadsad@mail.ru', '', '2019-04-19', 0),
-(4, 'Крейдо Игорь Витальевич', '', '', 'urFace', 'addr@list.ru', '', '2019-04-19', 0),
-(5, 'tom', '', '', 'urFace', 'addr@list.ru', '', '2019-04-19', 0),
-(6, 'mery', 'inn', '', 'urFace', 'addr@list.ru', '', '2019-04-19', 0),
-(7, 'polly', 'inn', '23345', 'urFace', 'autodesk@list.ru', '', '2019-04-19', 0),
-(8, 'jhkjhkjhkj', '8937579', '', 'fizFace', 'jkhkjhjk@kjlk', '', '2019-04-19', 0),
-(9, 'jhkjhdsjh kjh kjhk ', '8767686', '', 'fizFace', 'lkjsdf@lkjl', '', '2019-04-19', 0);
+INSERT INTO `user` (`id`, `fio`, `inn`, `phone`, `type`, `email`, `password`, `date_reg`, `rnp`, `token`) VALUES
+(1, 'Крейдо Игорь Витальевич', '823276472', '', '', 'autodesk@list.ru', 'kudLlPUMwR', '2019-04-19', 0, ''),
+(2, 'jzh f difj sd hf ', '82327647253465', '', 'fizFace', 'ksjdshfkjask@jdkjf.ru', '', '2019-04-19', 0, ''),
+(3, 'Крейдо Игорь Витальевич', '', '', 'urFace', 'asadsad@mail.ru', '', '2019-04-19', 0, ''),
+(4, 'Крейдо Игорь Витальевич', '', '', 'urFace', 'addr@list.ru', '', '2019-04-19', 0, ''),
+(5, 'tom', '', '', 'urFace', 'addr@list.ru', '', '2019-04-19', 0, ''),
+(6, 'mery', 'inn', '', 'urFace', 'addr@list.ru', '', '2019-04-19', 0, ''),
+(7, 'polly', 'inn', '23345', 'urFace', 'autodesk@list.ru', '', '2019-04-19', 0, ''),
+(8, 'jhkjhkjhkj', '8937579', '', 'fizFace', 'jkhkjhjk@kjlk', '', '2019-04-19', 0, ''),
+(9, 'jhkjhdsjh kjh kjhk ', '8767686', '', 'fizFace', 'lkjsdf@lkjl', '', '2019-04-19', 0, ''),
+(10, '123', '123', '', 'fizFace', 'autodesk@list.ru', '', '2019-04-19', 0, ''),
+(11, 'hello', '12345', '', 'fizFace', 'autodesk@list.ru', 'LQtANKyevT', '2019-04-19', 0, ''),
+(12, 'Шадрин Дмитрий Андреевич', '55', '', 'fizFace', 'shadrindmitry1337@gmail.com', 'zaMwrcJyZt', '2019-04-20', 0, 'b43d44bc65312efd819a3252c7304d34'),
+(13, 'Шадрин Дмитрий Андреевич', '777', '', 'fizFace', 'shadrindmitry1337@gmail.com', 'djGIH57i6O', '2019-04-20', 0, '1');
 
 --
 -- Индексы сохранённых таблиц
@@ -171,6 +242,24 @@ ALTER TABLE `contract`
 -- Индексы таблицы `orgs`
 --
 ALTER TABLE `orgs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `rnp`
+--
+ALTER TABLE `rnp`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `support`
+--
+ALTER TABLE `support`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `support_chat`
+--
+ALTER TABLE `support_chat`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -210,10 +299,25 @@ ALTER TABLE `contract`
 ALTER TABLE `orgs`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
+-- AUTO_INCREMENT для таблицы `rnp`
+--
+ALTER TABLE `rnp`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT для таблицы `support`
+--
+ALTER TABLE `support`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT для таблицы `support_chat`
+--
+ALTER TABLE `support_chat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
 -- AUTO_INCREMENT для таблицы `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
